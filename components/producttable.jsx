76cbd871e903057ef248daf1dc817c41a63a0bb7
chechu776 <shameerchechu@gmail.com>
2025-08-09 10:59:08 +0530

    cursors done

--- components/producttable.jsx
+++ components/producttable.jsx
@@ -184,13 +184,13 @@ const Table = () => {
                         <div className="flex justify-between">
                             <button
                                 onClick={addProduct}
-                                className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
+                                className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700 hover:cursor-pointer"
                             >
                                 {editId ? "Update" : "Add"}
                             </button>
                             <button
                                 onClick={() => setShow(false)}
-                                className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600"
+                                className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 hover:cursor-pointer"
                             >
                                 Cancel
                             </button>
