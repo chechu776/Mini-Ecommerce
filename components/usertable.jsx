53f048a3ab57cc4da6bb5c41c915841b8d913318
chechu776 <shameerchechu@gmail.com>
2025-08-11 10:10:41 +0530

    table updated

--- components/usertable.jsx
+++ components/usertable.jsx
@@ -15,10 +15,10 @@ const Table = () => {
         }
     }, [message]);
     const getUser = async () => {
-            const response = await Axiosinstance.get("/admin/showAllUsers");
-            console.log(response);
-            setUser(response.data);
-        };
+        const response = await Axiosinstance.get("/admin/showAllUsers");
+        console.log(response);
+        setUser(response.data);
+    };
     useEffect(() => {
         getUser();
     }, []);
@@ -49,48 +49,51 @@ const Table = () => {
                     </button>
                 </div>
             )}
-            <table className="w-11/12 bg-gray-900 text-white rounded-t-lg shadow-lg">
-                <thead>
-                    <tr className="bg-gray-900">
-                        <th className="py-2 px-4 text-center">ID</th>
-                        <th className="py-2 px-4 text-center">Name</th>
-                        <th className="py-2 px-4 text-center">Image</th>
-                        <th className="py-2 px-4 text-center">Phone no</th>
-                        <th className="py-2 px-4 text-center">Email</th>
-                        <th className="py-2 px-4 text-center">Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {user.map((item, index) => (
-                        <tr
-                            key={index}
-                            className="bg-white hover:bg-gray-200 text-gray-800 hover:text-gray-900 font-medium border-b border-gray-300 text-center"
-                        >
-                            <td className="py-3 px-4">{index+1}</td>
-                            <td className="py-3 px-4">{item.name}</td>
-                            <td className="py-3 px-4">
-                                <img
-                                    className="w-20 h-20 rounded-2xl object-cover"
-                                    src={`http://localhost:3000/${item.image}`}
-                                    alt=""
-                                />
-                            </td>
-                            <td className="py-3 px-4">{item.phone}</td>
-                            <td className="py-3 px-4">{item.email}</td>
-                            <td className="py-3 px-4">
-                                <button
-                                    className={`px-3 py-1 font-semibold hover:cursor-pointer rounded text-white ${item.status === 'active'
-                                        ? 'bg-green-500 hover:bg-green-600'
-                                        : 'bg-red-500 hover:bg-red-600'
-                                        }`} onClick={() => handleStatus(item._id)}
-                                >
-                                    {item.status}
-                                </button>
-                            </td>
+            <div className=" h-160 overflow-y-auto rounded-lg [&::-webkit-scrollbar]:hidden ">
+
+                <table className="w-11/12 bg-gray-900 text-white rounded-t-lg shadow-lg">
+                    <thead>
+                        <tr className="bg-gray-900">
+                            <th className="py-2 px-4 text-center">ID</th>
+                            <th className="py-2 px-4 text-center">Name</th>
+                            <th className="py-2 px-4 text-center">Image</th>
+                            <th className="py-2 px-4 text-center">Phone no</th>
+                            <th className="py-2 px-4 text-center">Email</th>
+                            <th className="py-2 px-4 text-center">Status</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {user.map((item, index) => (
+                            <tr
+                                key={index}
+                                className="bg-white hover:bg-gray-200 text-gray-800 hover:text-gray-900 font-medium border-b border-gray-300 text-center"
+                            >
+                                <td className="py-3 px-4">{index + 1}</td>
+                                <td className="py-3 px-4">{item.name}</td>
+                                <td className="py-3 px-4">
+                                    <img
+                                        className="w-20 h-20 rounded-2xl object-cover"
+                                        src={`http://localhost:3000/${item.image}`}
+                                        alt=""
+                                    />
+                                </td>
+                                <td className="py-3 px-4">{item.phone}</td>
+                                <td className="py-3 px-4">{item.email}</td>
+                                <td className="py-3 px-4">
+                                    <button
+                                        className={`px-3 py-1 font-semibold hover:cursor-pointer rounded text-white ${item.status === 'active'
+                                            ? 'bg-green-500 hover:bg-green-600'
+                                            : 'bg-red-500 hover:bg-red-600'
+                                            }`} onClick={() => handleStatus(item._id)}
+                                    >
+                                        {item.status}
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
