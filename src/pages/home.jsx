7e0d6a478817c1c39131dcea705dd01ff13144e8
chechu776 <shameerchechu@gmail.com>
2025-08-12 17:59:20 +0530

    home page added

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import Header from '../../components/Header'
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is the Home Page</h1>
-        </div>
+        <Header></Header>
     )
 }
 
