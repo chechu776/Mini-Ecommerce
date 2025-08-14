caa1bfe68d551e8f9d37f8b85951192863f082e5
chechu776 <shameerchechu@gmail.com>
2025-08-14 10:27:41 +0530

    imported slicker

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../components/Header'
 import Mainsection from '../../components/mainsection'
+import Product_listing from '../../components/product_listing'
 const Home = () => {
     return (
         <>
             <Header />
             <Mainsection />
+            <Product_listing />
         </>
     )
 }
