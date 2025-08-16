064e3cf4886326d49f98be88738e02533d74fc89
chechu776 <shameerchechu@gmail.com>
2025-08-16 12:31:08 +0530

    Footer Done

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="bg-gray-900 text-white py-4 text-center">
+            <p>Thank you for visiting My Ecommerce !!</p>
+        </footer>
+    );
+};
+
+export default Footer;
