770c19dec72abb191ac3b1d7630ab360a9370d51
chechu776 <shameerchechu@gmail.com>
2025-08-16 12:32:40 +0530

    added footer in homepage

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Header from '../../components/Header'
 import Mainsection from '../../components/mainsection'
-import Product_listing from '../../components/product_listing'
+import Product_listing from './product_listing'
+import Footer from '../../components/Footer'
 const Home = () => {
     return (
         <>
             <Header />
             <Mainsection />
             <Product_listing />
+            <Footer/>
         </>
     )
 }
