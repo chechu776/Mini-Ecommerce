cb581d933cb1c6f6eab53877b6ebab50b1fdf07c
chechu776 <shameerchechu@gmail.com>
2025-09-15 17:26:26 +0530

    search image compleeted

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -33,7 +33,7 @@ const SearchResults = () => {
                             className="bg-white shadow-md rounded-lg p-4 hover:shadow-lg transition-all"
                         >
                             <img
-                                src={`http://localhost:3000/${product.image}`}
+                                src={`${Axiosinstance.defaults.baseURL}/${product.image}`}
                                 alt={product.name}
                                 className="w-full h-48 object-cover rounded-lg mb-4"
                             />
